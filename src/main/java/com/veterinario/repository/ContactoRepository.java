aace36787898f9b35013db2cc5f35788f06633bd
krouser2 <61723296+krouser2@users.noreply.github.com>
2024-11-23 22:20:31 -0600

    se creo el crud de empleados y clientes se agregaron los links al menu de navegacion

--- src/main/java/com/veterinario/repository/ContactoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.veterinario.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.veterinario.entity.Contacto;
-
-public interface ContactoRepository extends JpaRepository<Contacto,Integer>{
-
-}

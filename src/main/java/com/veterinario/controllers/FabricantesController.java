4a1a3c13378ccb4ad90711debaaf13b32b72d23c
xReborn01 <diego.camacho.onti@gmail.com>
2024-11-24 22:44:28 -0600

    tratamiento

--- src/main/java/com/veterinario/controllers/FabricantesController.java
+++ src/main/java/com/veterinario/controllers/FabricantesController.java
@@ -1,21 +1,47 @@
 package com.veterinario.controllers;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.veterinario.entity.Fabricante;
+import com.veterinario.services.InFabricantesServices;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 
 @Controller
 @RequestMapping("/fabricantes")
 public class FabricantesController {
+    @Autowired
+     InFabricantesServices serviceFabricante;
+    
     @GetMapping("/")
-    public String index() {
-        return new String("fabricantes/lista-fabricantes");
-    }
+    public String listaFabricante(Model model){
+        List <Fabricante> lista=serviceFabricante.obtenerFabricantes();
+        model.addAttribute("fabricantes", lista);
+        return"fabricantes/lista-fabricantes";
+    };
 
     @GetMapping("/agregar")
     public String agregarFabricante() {
-        return new String("fabricantes/form-fabricantes");
+        return "fabricantes/form-fabricantes";
+    }
+
+@GetMapping("/eliminar")
+    public String eliminar(@RequestParam("id") int idFabricante) {
+        serviceFabricante.eliminarFabricante(idFabricante);
+        return new String("redirect:/fabricantes/");
+    }
+
+    @GetMapping("/actualizar")
+    public String actualizar(@RequestParam("id") int idFabricante, Model model) {
+        Fabricante fabricante = serviceFabricante.buscarFabricante(idFabricante);
+        model.addAttribute("fabricante", fabricante);
+        return new String("fabricantes/form-update-fabricante");
     }
 }
--- src/main/java/com/veterinario/controllers/TratamientosController.java
+++ src/main/java/com/veterinario/controllers/TratamientosController.java
@@ -1,18 +1,44 @@
 package com.veterinario.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.veterinario.entity.Empleado;
+import com.veterinario.entity.Tratamiento;
+import com.veterinario.services.InTratamientosServices;
 
 @Controller
 @RequestMapping("/tratamientos")
 public class TratamientosController {
+    @Autowired
+    InTratamientosServices servicesTratamientos;
     @GetMapping("/")
-    public String listaTratamientos(){
+    public String listaTratamiento(Model model){
+        List<Tratamiento> lista= servicesTratamientos.obtenerTratamientos();
+        model.addAttribute("tratamientos", lista);
         return new String("tratamientos/lista-tratamientos");
     }
     @GetMapping("/agregar")
     public String agregarTratamientos(){
         return new String("tratamientos/form-tratamiento");
     }
+    @GetMapping("/eliminar")
+    public String eliminar(@RequestParam("id") int idEmpleado) {
+        servicesTratamientos.eliminar(0);
+        return new String("redirect:/empleados/");
+    }
+    @PostMapping("/guardar")
+    public String guardar(Tratamiento tratamiento) {
+        servicesTratamientos.guardar(tratamiento);
+        return new String("redirect:/Tratamientos/");
+    }
+
+
 }
